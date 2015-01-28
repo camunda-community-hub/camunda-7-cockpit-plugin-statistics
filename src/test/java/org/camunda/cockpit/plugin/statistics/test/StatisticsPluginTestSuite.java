473c692114c3e18aba94ba57f7722cf6a5ded29b
Eric Klieme <eric.klieme@novatec-gmbh.de>
2015-01-28 10:07:11 +0100

    docs

--- src/test/java/org/camunda/cockpit/plugin/statistics/test/StatisticsPluginTestSuite.java
+++ src/test/java/org/camunda/cockpit/plugin/statistics/test/StatisticsPluginTestSuite.java
@@ -8,7 +8,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 /*
- * Depending on your running databases add or remove classes from the test suite
+ * Depending on your running databases add or remove classes from the test suite and put a @Ignore in the respective class
  */
 @RunWith(Suite.class)
 @SuiteClasses({DB2QueryTest.class, MySQLQueryTest.class, H2QueryTest.class, StatisticsPluginTest.class})
