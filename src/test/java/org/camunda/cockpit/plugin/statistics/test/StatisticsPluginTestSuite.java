24c4245d963e6eb38f35281881bbd935fedc8ef1
Eric Klieme <ekl@novatec-gmbh.de>
2015-03-24 11:12:06 +0100

    improve: added maven profile for testing several dbs in addition to built-in h2

--- src/test/java/org/camunda/cockpit/plugin/statistics/test/StatisticsPluginTestSuite.java
+++ src/test/java/org/camunda/cockpit/plugin/statistics/test/StatisticsPluginTestSuite.java
@@ -2,6 +2,7 @@ package org.camunda.cockpit.plugin.statistics.test;
 
 import org.camunda.cockpit.plugin.statistics.test.query.DB2QueryTest;
 import org.camunda.cockpit.plugin.statistics.test.query.H2QueryTest;
+import org.camunda.cockpit.plugin.statistics.test.query.MSSQLQueryTest;
 import org.camunda.cockpit.plugin.statistics.test.query.MySQLQueryTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -11,7 +12,7 @@ import org.junit.runners.Suite.SuiteClasses;
  * Depending on your running databases add or remove classes from the test suite and put a @Ignore in the respective class
  */
 @RunWith(Suite.class)
-@SuiteClasses({DB2QueryTest.class, MySQLQueryTest.class, H2QueryTest.class, StatisticsPluginTest.class})
+@SuiteClasses({DB2QueryTest.class, MySQLQueryTest.class, H2QueryTest.class, MSSQLQueryTest.class, StatisticsPluginTest.class})
 public class StatisticsPluginTestSuite {
 
 }
