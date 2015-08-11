337ba9a05cd1919f5678b2a83b6ed2e7ebf10a77
Ingo Gühring <ingo.guehring@novatec-gmbh.de>
2015-08-11 23:44:33 +0200

    improve(regression Plot): change how directive gets x and y values of data. refactor:keep all interesting attributes when formatting data for plot
    -regression directive now accepts an x and a y property in options that
    specifies how to get the values for the x and y axis. So the data has no
    longer to be in the form x:, y:
    -So far every time a different plot was chosen, the data had to be
    reformated, to reassign x and y attribute. Now data keeps all important
    attributes and getX and getY in plots is modified

--- src/main/resources/plugin-webapp/statistics-plugin/app/services/TimingFactory.js
+++ src/main/resources/plugin-webapp/statistics-plugin/app/services/TimingFactory.js
@@ -7,7 +7,7 @@ ngDefine('cockpit.plugin.statistics-plugin.services', function(module) {
 			return DataFactory.getActivityNamesTypesProcDefinition()
 			.then(function () {
 				var menuData = Format.bringSortedDataInPlotFormat
-					(DataFactory.activityNamesTypesProcDefinition,"procDefKey","type","activityName",undefined,undefined);
+				(DataFactory.activityNamesTypesProcDefinition,"procDefKey","type","activityName",undefined,undefined);
 				for(var i = 0; i< menuData.length;i++){
 					menuData[i].values = Format.bringNotSortedDataInPlotFormat(menuData[i].values,"x","y",undefined,undefined,undefined);
 					var j = DataFactory.activityNamesTypesProcDefinition.map(function(e) { return e.procDefKey; }).indexOf(menuData[i].key);
@@ -17,75 +17,77 @@ ngDefine('cockpit.plugin.statistics-plugin.services', function(module) {
 				TimingFactory.menuData = menuData;
 			});
 		};
-		
-		
+
+
 		var getTimeFormatAndParser = function(timeFrame){
 			if(timeFrame==="daily")
 				return {"format" : "%H:%M", "parser": eval("Format.breakDateDownTo24h") };
-			else if(timeFrame==="weekly")
-				return {"format" : "%a %H:%M", "parser": eval("Format.breakDateDownToWeek") };
-			else if(timeFrame === "noFrame")
-				return {"format": "%Y-%m-%dT%H", "parser": d3.time.format("%Y-%m-%dT%H:%M:%S").parse };
-			else console.debug("Error: no known time frame was chosen")
+				else if(timeFrame==="weekly")
+					return {"format" : "%a %H:%M", "parser": eval("Format.breakDateDownToWeek") };
+					else if(timeFrame === "noFrame")
+						return {"format": "%Y-%m-%dT%H", "parser": d3.time.format("%Y-%m-%dT%H:%M:%S").parse };
+						else console.debug("Error: no known time frame was chosen")
 		};
 
 		TimingFactory.data = [];
 		TimingFactory.options = [];
 		
-		TimingFactory.prepareData = function(rawData,options,colorScale){
+		
+		TimingFactory.prepareData = function(formatedData, options, colorScale) {
 			if(options.propertyToPlot == "regression"){
 				var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%S").parse;
-				TimingFactory.chosenData = Format.bringSortedDataInPlotFormat(rawData,["activityName","processDefinitionKey"],"startTime","durationInMillis",parseDate,function(d){return d/1000/60;});
 				TimingFactory.options  = {
-						outerRegion:[5,95],
+						outerRegion:[5, 95],
 						scatter  : options.showScatter,
 						regression : options.showRegression,
 						spline : options.showSplines,
+						x : "startTime",
+						y: "durationInMillis",
 						//TODO If it works put it all in chart
-						chart : {}
+						chart : {
+							color: colorScale
+						}
 				};
+				TimingFactory.parseX = parseDate;
+				TimingFactory.parseY = function(d) { return d/1000/60;};
+				TimingFactory.dataForPlot = formatedData;
 			}
-			else if(options.propertyToPlot == "startEndTime"){
+			else if(options.propertyToPlot == "startEndTime") {
 				var formatAndParser = getTimeFormatAndParser(options.timeFrame);
-				TimingFactory.chosenData = Format.bringSortedDataInPlotFormat(rawData,["activityName","processDefinitionKey"] ,options.time,"",formatAndParser.parser);
-				if(options.cluster.algo == "kmeans")
-					TimingFactory.chosenData = Format.getKMeansClusterFromFormatedData(TimingFactory.chosenData,options.cluster.numberOfClusters);
-				TimingFactory.options = GraphFactory.getOptionsForStartEndTimeGraph(formatAndParser.format, options.cluster.algo == "kmeans", 1000);
+//				if(options.cluster.algo == "kmeans")
+//					TimingFactory.chosenData = Format.getKMeansClusterFromFormatedData(TimingFactory.chosenData, options.cluster.numberOfClusters);
+				TimingFactory.options = GraphFactory.getOptionsForStartEndTimeGraph(formatAndParser, options.cluster.algo == "kmeans", 1000, options.time, colorScale);
+				TimingFactory.dataForPlot = formatedData;
 			}
 			else {
-				var dataAndBins = Format.bringDataIntoBarPlotFormat(rawData,["activityName","processDefinitionKey"],"durationInMillis",function(d){return d/1000/60;},options.numberOfBins);
-				TimingFactory.chosenData = dataAndBins.data;
-				TimingFactory.options = GraphFactory.getOptionsForTimeDistributionGraph(options.numberOfBins,dataAndBins.thresholds);
+				var dataAndBins = Format.bringDataIntoBarPlotFormat(formatedData, "durationInMillis", options.numberOfBins);
+				TimingFactory.dataForPlot = dataAndBins.data;
+				TimingFactory.options = GraphFactory.getOptionsForTimeDistributionGraph(options.numberOfBins, dataAndBins.thresholds, function(d){return d/1000/60;}, colorScale);
 			}
-			TimingFactory.options.chart.color = colorScale;
-			return {"data" : TimingFactory.chosenData , "options": TimingFactory.options};
+			return {"data" : TimingFactory.dataForPlot , "options": TimingFactory.options};
 		}
-		
+
 		/**
 		 * updates data without a call to the database
 		 */
 		TimingFactory.updateCharts = function(options){
-			return TimingFactory.prepareData(TimingFactory.data,options,TimingFactory.options.chart.color);
+			return TimingFactory.prepareData(TimingFactory.formatedData, options, TimingFactory.options.chart.color);
 		};
 		/**
 		 * @selectedFromMenu: processes, and activities chosen by the user to plot
-		 * @xValue: either start time or end time of the processes will be plotted on the x Axis
-		 * @timeFrame: either focus on weekly dates i.e. the rest of the date will be ignored
-		 * or focus on time only i.e. weekday, year will be ignored
-		 * @date: an object containing date.to a date, date.from a date
-		 * if specified only procs/acts in this period are plotted
+		 * @processData{boolean} in case we only request the data to get more information about the cluster range it 
+		 * will not be processed.
 		 */
 		/**
 		 * formating is now happening inside the loop, if performance becomes an issue we have to think of sth new
 		 */
-		TimingFactory.getModelMenuData = function(selectedFromMenu,options){
+		TimingFactory.getModelMenuData = function(selectedFromMenu, options){
 			console.log(options);
 			//make an iterator over the 10 d3 default colors
 			//colorDictionary is a map which assigns each key a color. This map is used to make 
 			//a color function for the plots. It is calculated here and then reused in each update call
 			//".color" properties of the legend are also set here. since changing something in selectedFromMenu also affects
 			//the view (since it is $scope.selected)
-			//TODO if more then 10 are selected we need the 20 color scale
 			var colorIterator = makeIterator(d3.scale.category20().range());
 			var colorDictionary =[];
 			angular.forEach(selectedFromMenu, function(processObject){
@@ -106,20 +108,36 @@ ngDefine('cockpit.plugin.statistics-plugin.services', function(module) {
 			})
 			//the colorfunction used in the plots
 			var colorScale = function(d, i) {
-		        keyIndex = colorDictionary.map(function(e) { return e.key; }).indexOf(d.key);
-		        return colorDictionary[keyIndex].color;
-		    };
+				keyIndex = colorDictionary.map(function(e) { return e.key; }).indexOf(d.key);
+				return colorDictionary[keyIndex].color;
+			};
 			return DataFactory.getDataFromModelMenu(selectedFromMenu,options.timeWindow)
 			.then(function(promiseData){
-				console.log(promiseData);
 				TimingFactory.data  =[];
 				angular.forEach(promiseData, function(singleCallbackReturn,promiseIndex){
 					TimingFactory.data = TimingFactory.data.concat(singleCallbackReturn.data);
 				});
-				TimingFactory.prepareData(TimingFactory.data,options,colorScale);
+//				//if we only want the data to know the ranges for the clustering, then this will be false
+//				if(processData)
+				TimingFactory.formatedData = Format.bringSortedDataInKeyFormat(TimingFactory.data, ["activityName","processDefinitionKey"], 
+						["processDefinitionId", "processDefinitionKey", "activityId", "activityName", "startTime", "endTime", "durationInMillis"]);
+				TimingFactory.numberOfInstancesMap = getNumberOfInstances(TimingFactory.formatedData);
+				TimingFactory.prepareData(TimingFactory.formatedData, options, colorScale);
 			});
 		};
+
 		
+		var getNumberOfInstances = function(formatedData) {
+			var numberOfInstancesMap = {};
+			for (var i = 0; i < formatedData.length; i++) {
+				var numberOfEndedInst = formatedData[i].values.map(function(d) { return d["endTime"] == null? null : d}).filter(function(d) { return d != null }).length;
+				console.log(formatedData[i].values);
+				numberOfInstancesMap[formatedData[i].key] = {"startedInst": formatedData[i].values.length , 
+						"endedInst": numberOfEndedInst };
+			}
+			console.log(numberOfInstancesMap);
+			return numberOfInstancesMap;
+		}
 		/**
 		 * @return: an iterator over an array. When the end is reached it starts all over again
 		 * it is used to iterate over one of d3´s standard color arrays and make our own legend
@@ -136,7 +154,7 @@ ngDefine('cockpit.plugin.statistics-plugin.services', function(module) {
 				}
 			}
 		};
-		
+
 		return TimingFactory;
 	}]);
 });
