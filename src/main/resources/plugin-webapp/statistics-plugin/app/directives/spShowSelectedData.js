2d89a36401d3c272b1cab8e32d7f620fca8c5fcf
Ingo Gühring <ingo.guehring@novatec-gmbh.de>
2015-08-15 13:11:44 +0200

    feat(clustering): choose default values for clustering with rule of thumb

--- src/main/resources/plugin-webapp/statistics-plugin/app/directives/spShowSelectedData.js
+++ src/main/resources/plugin-webapp/statistics-plugin/app/directives/spShowSelectedData.js
@@ -3,16 +3,35 @@
  */
 ngDefine('cockpit.plugin.statistics-plugin.directives', function(module) {
 	module.directive('spShowSelectedData', function(Uri){
-		 return {
-		      restrict: 'E',
-		      scope: {
-		    	  selectedData: '=',
-		    	  showLegend: '=',
-		    	  showSlider: '=',
-		    	  numberOfInstancesMap: '=',
-		    	  removeFct: '&'
-		      },
-		      templateUrl: require.toUrl(Uri.appUri('plugin://statistics-plugin/static/app/partials/sp-show-selected-data.html'))
-		    };		
+
+		var controller = function ($scope) {
+			$scope.init = function() {
+				console.log($scope.numberOfInstancesMap);
+				angular.forEach($scope.numberOfInstancesMap, function(instanceObject) {
+					instanceObject.numberOfClusters = Math.floor( Math.sqrt(instanceObject.startedInst));
+				});
+			}
+		};
+
+		var link = function(scope, element, attr, ctrl) {
+			scope.$watch('numberOfInstancesMap',function(){
+				console.log("watch firired");
+				scope.init();
+			});
+		};
+
+		return {
+			restrict: 'E',
+			scope: {
+				selectedData: '=',
+				showLegend: '=',
+				showSlider: '=',
+				numberOfInstancesMap: '=',
+				removeFct: '&'
+			},
+			controller: controller,
+			link: link,
+			templateUrl: require.toUrl(Uri.appUri('plugin://statistics-plugin/static/app/partials/sp-show-selected-data.html'))
+		};		
 	});
 });
