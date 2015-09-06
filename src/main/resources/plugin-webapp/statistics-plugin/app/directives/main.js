971dc7626c8e0a0e4f3ef9125cc5fd9a481cbd85
kerstinguenther <Kerstin.Guenther@student.hpi.uni-potsdam.de>
2015-09-06 17:10:21 +0200

    created own directive for navigator chart

--- src/main/resources/plugin-webapp/statistics-plugin/app/directives/main.js
+++ src/main/resources/plugin-webapp/statistics-plugin/app/directives/main.js
@@ -2,7 +2,8 @@ ngDefine('cockpit.plugin.statistics-plugin.directives', [
     //declare dependencies to your own directives here                                                     
     './regressionPlot',
     './startEndControlElement',
-    '../process-diagram-overlay/directives/activityDurationChart'
+    '../process-diagram-overlay/directives/durationChart',
+    '../process-diagram-overlay/directives/navigatorChart'
 ], function(module) {
 
 });
