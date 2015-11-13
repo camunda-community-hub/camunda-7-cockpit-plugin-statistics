d5683daf1ac7e4f9bb71b62be02baf250b737c04
Eric Klieme <ekl@novatec-gmbh.de>
2015-11-13 15:38:03 +0100

    fix(dependencies): nvd3 does not find d3.js file while initial app bootstrapping

--- src/main/resources/plugin-webapp/statistics-plugin/app/directives/main.js
+++ src/main/resources/plugin-webapp/statistics-plugin/app/directives/main.js
@@ -1,13 +1,14 @@
 ngDefine('cockpit.plugin.statistics-plugin.directives', [
-    //declare dependencies to your own directives here                                                     
+    //declare dependencies to your own directives here
+  '../lib/nv.d3.own',
   './spRegressionPlot',
   './spInput',
   './spMenu',
   './spPlotInfo',
   './spShowSelectedData',
   './spDropDownMultiSelect',
-    '../process-diagram-overlay/directives/durationChart',
-    '../process-diagram-overlay/directives/navigatorChart'
+  '../process-diagram-overlay/directives/durationChart',
+  '../process-diagram-overlay/directives/navigatorChart'
 ], function(module) {
 
 });
