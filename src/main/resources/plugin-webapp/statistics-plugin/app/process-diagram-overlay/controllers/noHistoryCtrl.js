11ad75350be8670d9f9a1e8ddab5499534c31cc2
kerstinguenther <Kerstin.Guenther@student.hpi.uni-potsdam.de>
2015-12-04 17:40:16 +0100

    loading window; fixed navigator chart and calendar popup in chrome

--- src/main/resources/plugin-webapp/statistics-plugin/app/process-diagram-overlay/controllers/noHistoryCtrl.js
+++ src/main/resources/plugin-webapp/statistics-plugin/app/process-diagram-overlay/controllers/noHistoryCtrl.js
@@ -3,6 +3,10 @@ ngDefine('cockpit.plugin.statistics-plugin.controllers', function(module) {
 
 	module.controller('noHistoryCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance){
 
+		$scope.init = function() {
+			$rootScope.$broadcast("init");
+		}
+		
 		$scope.closeModal = function() {
 			$modalInstance.close();
 		}
