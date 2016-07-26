2305582e647b5375f3be233ed5f6a9da87ef342c
kerstinguenther <Kerstin.Guenther@student.hpi.uni-potsdam.de>
2016-07-26 16:44:44 +0200

    FIX: process diagram settings modal is working with Camunda BPM 7.5 now; modal is closed when navigating to another page

--- src/main/resources/plugin-webapp/statistics-plugin/app/process-diagram-overlay/controllers/overlayMenuCtrl.js
+++ src/main/resources/plugin-webapp/statistics-plugin/app/process-diagram-overlay/controllers/overlayMenuCtrl.js
@@ -1,27 +1,46 @@
 'use strict'
 ngDefine('cockpit.plugin.statistics-plugin.controllers', function(module) {
 
-	module.controller('overlayMenuCtrl', ['$scope', '$modal', function($scope, $modal){
+	module.controller('overlayMenuCtrl', ['$scope', '$rootScope', '$modal', 'DataFactory', function($scope, $rootScope, $modal, DataFactory){
 
 		var modalInstance = null;
 
-		$scope.toggleMenu = function() {
-
+		$rootScope.$on("$routeChangeStart", function(args){
+			console.log("route changed...");
+			if(modalInstance) {
+  			closeModal();
+  		}
+		});
+		
+		$scope.toggleMenu = function(event) {
+			event.preventDefault();
+			event.stopPropagation();
 			if(!modalInstance) {
 				modalInstance = $modal.open({
 					templateUrl: 'processDiagramSettingsModal.html',
 					controller: 'processDiagramSettingsCtrl',
 					size: 'sm',
-					windowClass: 'modal-right'
+					windowClass: 'modal-right',
+					backdrop: false
+				});
+				modalInstance.result.then(function() {
+					closeModal()
+				}, function() {
+					closeModal()
 				});
 			} else {
-				modalInstance.close();
-				modalInstance = null;
+				closeModal();
 			}
-
-
 		};
 
+		function closeModal() {
+			modalInstance.close();
+			modalInstance = null;
+			DataFactory.bpmnElementsToHighlight = {};
+			DataFactory.bpmnElementsToHighlightAsWarning = {};
+			DataFactory.activityDurations = {};
+		}
+		
 	}]);
 
 });
